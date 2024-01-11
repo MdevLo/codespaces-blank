baba1f12699ac7e21ebac92bfd63fe5ea7485ced
MdevLo <111896522+MdevLo@users.noreply.github.com>
2024-01-11 00:33:50 +0000

    main

--- travelvisor/src/components/Map/Map.jsx
+++ travelvisor/src/components/Map/Map.jsx
@@ -1,7 +1,32 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@mui/material'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Rating from '@mui/material/Rating';
+
+import './styles.css'
+
 const Map = () => {
+  const isMobile = useMediaQuery('(min-width: 600px)')
+
+  const coordinates = { lat: 0, lng: 0 }
+
     return(
-        <div>
-            Map
+        <div className='mapContainer'>
+            <GoogleMapReact
+              bootstrapURLKeys={{key: ''}}
+              defaultCenter={coordinates}
+              center={coordinates}
+              defaultZoom={14}
+              margin={[50, 50, 50, 50]}
+              options={''}
+              onChange={''}
+              onChildClick={''}
+            
+            
+            >
+
+            </GoogleMapReact>
         </div>
     )
 }
