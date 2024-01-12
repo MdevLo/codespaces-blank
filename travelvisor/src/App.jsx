c5cbb8bead08ce9a9f4d16c66f4dd4515a4c9e7e
MdevLo <111896522+MdevLo@users.noreply.github.com>
2024-01-12 02:21:10 +0000

    commit

--- travelvisor/src/App.jsx
+++ travelvisor/src/App.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
 import { CssBaseline, Grid } from '@mui/material'
+import { getPlacesData } from './api/index'
 
 
 import Header from './components/Header/Header'
@@ -7,7 +8,28 @@ import List from './components/List/List'
 import Map from './components/Map/Map'
 
 
+
 const App = () => {
+
+  const [ places, setPlaces ] = useState([])
+  const [ coordinates, setCoordinates] = useState({})
+  const [ bounds, setBounds ] = useState(null)
+
+  useEffect(() =>{
+    navigator.geolocation.getCurrentPosition(({ coords:{latitude, longitude} }) =>{
+      setCoordinates({ lat: latitude , lng: longitude})
+    })
+  }, [])
+
+  useEffect(() =>{
+    getPlacesData(bounds.sw, bounds.ne)
+        .then((data) =>{
+          console.log(data)
+          setPlaces(data)
+        })
+  }, [coordinates, bounds])  
+
+
   return(
     <>
      <CssBaseline />
@@ -17,7 +39,11 @@ const App = () => {
           <List />
         </Grid>
         <Grid item xs={12} md={4}>
-          <Map />
+          <Map 
+           setCoordinates={setCoordinates}
+           setBounds={setBounds}
+           coordinates={coordinates}
+          />
         </Grid>
      </Grid>
     </>
--- travelvisor/src/components/Map/Map.jsx
+++ travelvisor/src/components/Map/Map.jsx
@@ -1,34 +1,27 @@
-import React from 'react'
-import GoogleMapReact from 'google-map-react'
-import { Paper, Typography, useMediaQuery } from '@mui/material'
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import Rating from '@mui/material/Rating';
+import React from "react"
+import RadarMap from "../renderMap/radarMap"
 
-import './styles.css'
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
+  
+  return(
+   <div>
+     <RadarMap
+      defaultCenter={coordinates}
+      center={coordinates}
+      defaultZoom={14}
+      margin={[50, 50, 50, 50]}
+      options={''}
+      onChange={(e) =>{
+        console.log(e)
+        setCoordinates({ lat: e.center.lat, lng: e.center.lng })
+        setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+      }}
+      onChildClick={''}
+     >
 
-const Map = () => {
-  const isMobile = useMediaQuery('(min-width: 600px)')
-
-  const coordinates = { lat: 0, lng: 0 }
-
-    return(
-        <div className='mapContainer'>
-            {/* <GoogleMapReact
-              bootstrapURLKeys={{key: ''}}
-              defaultCenter={coordinates}
-              center={coordinates}
-              defaultZoom={14}
-              margin={[50, 50, 50, 50]}
-              options={''}
-              onChange={''}
-              onChildClick={''}
-            
-            
-            >
-
-            </GoogleMapReact> */}
-        </div>
-    )
+     </RadarMap>
+   </div>
+  )
+ 
 }
-
 export default Map
